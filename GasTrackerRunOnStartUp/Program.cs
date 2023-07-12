00de9c3bd8847ebe1bde6f9f8df5e6eaebd54f4c
Ryaza5562 <49323900+Ryaza5562@users.noreply.github.com>
2023-07-12 15:30:10 +0300

    Upload subprogram

---
+++ GasTrackerRunOnStartUp/Program.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Win32.TaskScheduler;
+
+namespace GasTrackerRunOnStartUp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            if (args.Length == 2)
+            {
+                if (args[0] == "add")
+                {
+                    try
+                    {
+                        TaskDefinition td = TaskService.Instance.NewTask();
+                        td.RegistrationInfo.Description = "Gas tracker in all EVM chains";
+                        td.RegistrationInfo.Author = "Ryaza";
+                        td.Triggers.Add(new BootTrigger { Delay = TimeSpan.FromSeconds(3) });
+                        td.Actions.Add(new ExecAction(args[1]));
+                        TaskService.Instance.RootFolder.RegisterTaskDefinition("GasTracker", td);
+                    }
+                    catch (Exception e) 
+                    {
+                        using (StreamWriter sw = new StreamWriter(path + "\\" + "log.txt")) 
+                        {
+                            sw.WriteLine(e.ToString());
+                        }
+                    }
+                }
+                else if (args[0] == "delete")
+                {
+                    try
+                    {
+                        TaskService.Instance.RootFolder.DeleteTask("GasTracker");
+                    }
+                    catch (Exception e) 
+                    {
+                        using (StreamWriter sw = new StreamWriter(path + "\\" + "log.txt"))
+                        {
+                            sw.WriteLine(e.ToString());
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
